0ebfddce27e5a6ae925c75235a7abe9546f3ca2a
GiuseppeCalderonio <giuseppe.calderonio@mail.polimi.it>
2023-12-10 13:26:09 +0100

    Gnenerator completed

--- compile.sh
+++ compile.sh
@@ -3,8 +3,16 @@
 cvs=find . -name 'CV-*.tex'
 
 for FILE in CV-*.tex; do
-    pdflatex ${FILE}
-    mv -- "$FILE" "${FILE%.tex}.pdf" 
-    mv ${FILE} ../ 
+    # Compile the LaTeX file to produce a PDF
+    pdflatex "$FILE"
+    
+    # Get the filename without the extension
+    filename=$(basename -- "$FILE")
+    filename_no_ext="${filename%.tex}"
+    
+    # Move the produced PDF file to the parent directory
+    mv "${filename_no_ext}.pdf" ../
 done
+
+rm -f *.log *.aux *.out
 
